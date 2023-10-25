55cd7fa4d17242705a8fc9d56599096ac3ab4f8d
unknown <gausulislam5@gmail.com>
2023-10-25 20:22:28 +0600

    CRUD er delete system update

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -4,8 +4,19 @@ import { useLoaderData } from 'react-router-dom';
 const User = () => {
     const users=useLoaderData();
 
-    const handleDelete=()=>{
-        
+    const handleDelete=(_id)=>{
+        console.log('delete',_id);
+        fetch(`http://localhost:5000/users/${_id}`,{
+            
+        method:'DELETE'
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+            if (data.deleteCount > 0) {
+                alert('delete successfully');
+            }
+        })
     }
     return (
         <div>
@@ -14,7 +25,7 @@ const User = () => {
             {
                 users.map(user=>
                 <p key={user._id}>
-                    {user.name}: {user.email}<button onClick={handleDelete} >
+                    {user.name}: {user.email}<button onClick={()=>handleDelete(user._id)} >
                         X</button></p>)
             }
            </div>
